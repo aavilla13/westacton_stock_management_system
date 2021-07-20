bd51fcb041a8a3704850329c90b46bf14fe8b802
aavilla13 <45303537+aavilla13@users.noreply.github.com>
2021-07-20 15:04:12 +0800

    Database structure

---
+++ westacton_stock_management_system.sql
@@ -0,0 +1,50 @@
+/*
+SQLyog Ultimate v11.11 (32 bit)
+MySQL - 8.0.25 : Database - westacton_stock_management_system
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`westacton_stock_management_system` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `westacton_stock_management_system`;
+
+/*Table structure for table `product` */
+
+CREATE TABLE `product` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(100) NOT NULL,
+  `product_qty` int DEFAULT NULL,
+  `product_price` float DEFAULT NULL,
+  `added_when` datetime DEFAULT NULL,
+  `added_by` varchar(50) DEFAULT NULL,
+  `updated_when` datetime DEFAULT NULL,
+  `updated_by` varchar(50) DEFAULT NULL,
+  `deleted_when` datetime DEFAULT NULL,
+  `deleted_by` varchar(50) DEFAULT NULL,
+  `deleted` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+
+/*Table structure for table `sales` */
+
+CREATE TABLE `sales` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `product_id` int DEFAULT NULL,
+  `sales_qty` int DEFAULT NULL,
+  `sales_total` float DEFAULT NULL,
+  `added_when` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
